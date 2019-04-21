32b77975965709e3296a35f55b49f2a1349f1be7
Alberto Castañeda <albertcastaned@gmail.com>
2019-04-21 01:06:52 -0500

    Multithread DOLFIN 2

--- multi.py
+++ multi.py
@@ -105,6 +105,54 @@ def moverMotor(threadID,pos,v):
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.HIGH)
             time.sleep(v)
+        elif(threadID == "Thread-3"):
+            GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.HIGH)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(29, GPIO.LOW)
+            GPIO.output(26, GPIO.HIGH)
+            time.sleep(v)
         count+=1
         print(str(threadID) + " ,Step # " + str(count))
         time.sleep(v)
@@ -124,13 +172,18 @@ GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
+
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 k=0
 v=.0005
 
 for i in range(0,int(len(lines)/6)):
     print("Set " + str(i + 1))
     threads = []
-    for j in range(0,2):
+    for j in range(0,3):
         t = threading.Thread(target=moverMotor, args=("Thread-" + str(j + 1),j + posicion,v))
         threads.append(t)
         t.start()
