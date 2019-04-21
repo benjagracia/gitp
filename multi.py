66f8fd1e794757a40e7dee452474889798d09d05
Alberto Castañeda <albertcastaned@gmail.com>
2019-04-21 01:52:49 -0500

    Neo-Dolfin

--- multi.py
+++ multi.py
@@ -8,7 +8,7 @@ def moverMotor(threadID,pos,v):
     steps = int(lines[pos])
     while(count < steps):
         if(threadID == "Thread-1"):
-            GPIO.output(23, GPIO.HIGH)
+            """GPIO.output(23, GPIO.HIGH)
             GPIO.output(24, GPIO.LOW)
             GPIO.output(25, GPIO.LOW)
             GPIO.output(5, GPIO.LOW)
@@ -54,9 +54,9 @@ def moverMotor(threadID,pos,v):
             GPIO.output(24, GPIO.LOW)
             GPIO.output(25, GPIO.LOW)
             GPIO.output(5, GPIO.HIGH)
-            time.sleep(v)
+            time.sleep(v)"""
         elif(threadID == "Thread-2"):
-            GPIO.output(18, GPIO.HIGH)
+            """ GPIO.output(18, GPIO.HIGH)
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.LOW)
@@ -102,8 +102,152 @@ def moverMotor(threadID,pos,v):
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.HIGH)
-            time.sleep(v)
+            time.sleep(v) """
         elif(threadID == "Thread-3"):
+            """ GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.HIGH)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(6, GPIO.HIGH)
+            GPIO.output(13, GPIO.LOW)
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.HIGH)
+            time.sleep(v) """
+        elif(threadID == "Thread-4"):
+            GPIO.output(23, GPIO.HIGH)
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(25, GPIO.LOW)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.HIGH)
+            GPIO.output(24, GPIO.HIGH)
+            GPIO.output(25, GPIO.LOW)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.LOW)
+            GPIO.output(24, GPIO.HIGH)
+            GPIO.output(25, GPIO.LOW)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.LOW)
+            GPIO.output(24, GPIO.HIGH)
+            GPIO.output(25, GPIO.HIGH)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(25, GPIO.HIGH)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(25, GPIO.HIGH)
+            GPIO.output(5, GPIO.HIGH)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.LOW)
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(25, GPIO.HIGH)
+            GPIO.output(5, GPIO.LOW)
+            time.sleep(v)
+                
+            GPIO.output(23, GPIO.HIGH)
+            GPIO.output(24, GPIO.LOW)
+            GPIO.output(25, GPIO.LOW)
+            GPIO.output(5, GPIO.HIGH)
+            time.sleep(v)
+        elif(threadID == "Thread-5"):
+            GPIO.output(18, GPIO.HIGH)
+            GPIO.output(17, GPIO.LOW)
+            GPIO.output(27, GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.HIGH)
+            GPIO.output(17, GPIO.HIGH)
+            GPIO.output(27, GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(17, GPIO.HIGH)
+            GPIO.output(27, GPIO.LOW)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(17, GPIO.HIGH)
+            GPIO.output(27, GPIO.HIGH)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(17, GPIO.LOW)
+            GPIO.output(27, GPIO.HIGH)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(17, GPIO.LOW)
+            GPIO.output(27, GPIO.HIGH)
+            GPIO.output(22, GPIO.HIGH)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.LOW)
+            GPIO.output(17, GPIO.LOW)
+            GPIO.output(27, GPIO.HIGH)
+            GPIO.output(22, GPIO.LOW)
+            time.sleep(v)
+            
+            GPIO.output(18, GPIO.HIGH)
+            GPIO.output(17, GPIO.LOW)
+            GPIO.output(27, GPIO.LOW)
+            GPIO.output(22, GPIO.HIGH)
+            time.sleep(v)
+        elif(threadID == "Thread-6"):
             GPIO.output(6, GPIO.HIGH)
             GPIO.output(13, GPIO.LOW)
             GPIO.output(19, GPIO.LOW)
@@ -151,7 +295,6 @@ def moverMotor(threadID,pos,v):
             GPIO.output(19, GPIO.LOW)
             GPIO.output(26, GPIO.HIGH)
             time.sleep(v)
-            #print(str(threadID) + " ,Step # " + str(count))
         count+=1
         print(str(threadID) + " ,Step # " + str(count))
         time.sleep(v)
@@ -182,7 +325,7 @@ threads = []
 for i in range(0,int(len(lines)/6)):
     print("Set " + str(i + 1))
     threads.clear()
-    for j in range(0,3):
+    for j in range(0,6):
         t = threading.Thread(target=moverMotor, args=("Thread-" + str(j + 1),j + posicion,v))
         threads.append(t)
         t.start()
