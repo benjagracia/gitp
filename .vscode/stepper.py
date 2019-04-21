f1f2490702c03811fce33c83f600b3d9e5d789cf
benjagracia <benjagraciafrancis@gmail.com>
2019-04-20 19:20:34 -0500

    pineapple2

--- .vscode/stepper.py
+++ .vscode/stepper.py
@@ -1,25 +1,55 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(2, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18 GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
-GPIO.output(12, GPIO.HIGH)
-GPIO.output(16, GPIO.HIGH)
-GPIO.output(18, GPIO.HIGH)
-GPIO.output(22, GPIO.HIGH)
+k=0
+while(k<512)
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(22, GPIO.HIGH)
+
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)
+
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(22, GPIO.HIGH)
+    k=k+1
 
 
 
-
-
-
-
-
-time.sleep(3)
-
-GPIO.output(18, GPIO.LOW)
 GPIO.cleanup()
